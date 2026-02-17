af9a8dee2bb6f4c2af1c590204411911b92a16a2
Brandon Flexman <112587570+FlexyRexy@users.noreply.github.com>
2026-02-16 17:42:57 -0700

    Add autostart-cli.sh for VM autostart setup

---
+++ autostart-cli.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ensure the user is running script as sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Try using sudo."
+   exit 1
+fi
+
+# Set the PVE virtual machine to autostart with the computer
+virsh autostart pve
+
+# Verify autostart status
+virsh dominfo vm_name | grep Autostart
+
+# Autostart the virtual network interface
+virsh net-autostart default
+
+# Disable the GUI and switch to CLI only
+systemctl set-default multi-user.target
